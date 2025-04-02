e4ad4fda010de4a67d0501c889c6c2d1ecf143d5
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-02 12:21:23 +0100

    feat: add QueriesExplorer component for listing and creating queries

---
+++ frontend/js/pages/QueriesExplorer.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { Button, Tab, Tabs } from "react-bootstrap";
+
+import { QueriesService, Query } from "../api";
+import QueryExplorer from "../components/QueryExplorer";
+
+const QueriesExplorer = () => {
+  const [queries, setQueries] =
+    useState<Awaited<ReturnType<typeof QueriesService.queriesList>>>();
+
+  const fetchQueries = async () => {
+    const result = await QueriesService.queriesList();
+    setQueries(result);
+  };
+
+  const createQuery = async (): Promise<void> => {
+    try {
+      await QueriesService.queriesCreate({
+        requestBody: {
+          text: "",
+        } as Query,
+      });
+
+      fetchQueries();
+    } catch (error) {
+      console.error("Error creating query:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQueries();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-center my-2">
+        <h5>Query Explorer</h5>
+        <Button size="sm" variant="primary" onClick={createQuery}>
+          New Query
+        </Button>
+      </div>
+      <Tabs className="mb-3" id="queries">
+        {queries &&
+          queries.results.map((query: Query) => (
+            <Tab key={query.id} eventKey={query.id} title={query.id}>
+              <QueryExplorer query={query} />
+            </Tab>
+          ))}
+      </Tabs>
+    </div>
+  );
+};
+
+export default QueriesExplorer;
