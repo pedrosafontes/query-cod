0e2ac92f93e9e6b589a49f73c0ca3426cd6aaa44
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-03 23:01:56 +0100

    feat: update Tailwind CSS primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./frontend/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.gray[950],
+        'primary-foreground': colors.white,
+      },
+    },
   },
   plugins: [],
 }
