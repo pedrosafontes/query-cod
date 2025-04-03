96fd70d177b81800b56ef7f4553f14a42fee8d79
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-03 23:12:15 +0100

    feat: change page size to 5 in DataTable component

--- frontend/js/components/DataTable.tsx
+++ frontend/js/components/DataTable.tsx
@@ -30,6 +30,11 @@ export function DataTable<TData, TValue>({
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 5,
+      },
+    },
   })
  
   return (
