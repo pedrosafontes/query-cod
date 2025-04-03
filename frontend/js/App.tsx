4b0753316e21528bd09a1e1372812195edf2c0c7
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-03 21:33:02 +0100

    feat: implement toast notifications with Toaster component and useToast hook

--- frontend/js/App.tsx
+++ frontend/js/App.tsx
@@ -3,6 +3,7 @@ import cookie from "cookie";
 
 import { OpenAPI } from "./api";
 import QueriesExplorer from "./pages/QueriesExplorer";
+import { Toaster } from "@/components/ui/toaster"
 
 OpenAPI.interceptors.request.use((request) => {
   const { csrftoken } = cookie.parse(document.cookie);
@@ -15,6 +16,7 @@ OpenAPI.interceptors.request.use((request) => {
 const App = () => (
   <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
     <QueriesExplorer />
+    <Toaster />
   </Sentry.ErrorBoundary>
 );
 
