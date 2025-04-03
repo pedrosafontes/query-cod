4089c3d1d7210070f9c90a97ee61f2f452acc9f5
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-03 21:33:51 +0100

    feat: update Button and Tabs components in QueriesExplorer

--- frontend/js/pages/QueriesExplorer.tsx
+++ frontend/js/pages/QueriesExplorer.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
-import { Button, Tab, Tabs } from "react-bootstrap";
 
 import { QueriesService, Query } from "../api";
 import QueryExplorer from "../components/QueryExplorer";
+import { Button } from "@/components/ui/button"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const QueriesExplorer = () => {
   const [queries, setQueries] =
     useState<Awaited<ReturnType<typeof QueriesService.queriesList>>>();
+  const [currentQueryId, setCurrentQueryId] = useState<string | undefined>(undefined);
+
+  const query = queries?.find(
+    (query) => query.id.toString() === currentQueryId
+  );
 
   const fetchQueries = async () => {
     const result = await QueriesService.queriesList();
@@ -32,20 +38,30 @@ const QueriesExplorer = () => {
   }, []);
 
   return (
-    <div className="container">
-      <div className="d-flex justify-content-between align-items-center my-2">
-        <Button size="sm" variant="primary" onClick={createQuery}>
-          New Query
-        </Button>
+    <div className="w-screen h-screen">
+      <div className="grid grid-cols-8 gap-4 h-full">
+        <div className="col-span-1 overflow-auto px-3 border-r py-5">
+          <div className="sticky top-0 z-10 mb-3 bg-white">
+            <Button variant="outline" onClick={createQuery} size="sm" className="w-full">
+              New Query
+            </Button>
+          </div>
+          <div className="overflow-y-auto flex-1 pr-1">
+            <Tabs value={currentQueryId} onValueChange={setCurrentQueryId}>
+              <TabsList className="flex flex-col h-auto w-full">
+                {queries && queries.map((query) => (
+                  <TabsTrigger key={query.id} value={query.id.toString()}>
+                    Query {query.id}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+            </Tabs>
+          </div>
+        </div>
+        <div className="col-span-7">
+          {query && <QueryExplorer query={query} key={query.id} />}
+        </div>
       </div>
-      <Tabs className="mb-3" id="queries">
-        {queries &&
-          queries.map((query: Query) => (
-            <Tab key={query.id} eventKey={query.id} title={query.id}>
-              <QueryExplorer query={query} />
-            </Tab>
-          ))}
-      </Tabs>
     </div>
   );
 };
