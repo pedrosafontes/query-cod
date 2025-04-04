596f2ffe86453fa234d2789cfd0aff36e9ee245a
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-04 12:11:07 +0100

    fix: add default case to reducer in use-toast hook

--- frontend/js/hooks/use-toast.ts
+++ frontend/js/hooks/use-toast.ts
@@ -123,6 +123,9 @@ export const reducer = (state: State, action: Action): State => {
         ...state,
         toasts: state.toasts.filter((t) => t.id !== action.toastId),
       };
+
+    default:
+      return state;
   }
 };
 
