bf7204d116a28045e66d459d472b11ab92210474
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-06 16:22:51 +0100

    feat: update Database model fields

--- backend/databases/models.py
+++ backend/databases/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 class Database(models.Model):
     class DatabaseType(models.TextChoices):
-        POSTGRESQL = 'postgresql'
-        MYSQL = 'mysql'
-        SQLITE = 'sqlite'
+        POSTGRESQL = 'postgresql', 'PostgreSQL'
+        MYSQL = 'mysql', 'MySQL'
+        SQLITE = 'sqlite', 'SQLite'
 
     name = models.CharField(max_length=255)
     description = models.TextField()
     host = models.CharField(max_length=255)
     port = models.IntegerField()
-    username = models.CharField(max_length=255)
+    user = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     database_name = models.CharField(max_length=255)
     type = models.CharField(
