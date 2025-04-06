4495e7c464be3cd05ff82fb130dce2034f14821f
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-06 16:25:51 +0100

    feat: update Database model to restrict type choices to PostgreSQL

--- backend/databases/models.py
+++ backend/databases/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Database(models.Model):
     class DatabaseType(models.TextChoices):
         POSTGRESQL = 'postgresql', 'PostgreSQL'
-        MYSQL = 'mysql', 'MySQL'
-        SQLITE = 'sqlite', 'SQLite'
 
     name = models.CharField(max_length=255)
     description = models.TextField()
