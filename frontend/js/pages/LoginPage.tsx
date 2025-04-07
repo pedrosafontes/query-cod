34fd2042b144c1a8ad9b536efae88fd934182194
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-07 22:29:16 +0100

    fix: import statement for Login

--- frontend/js/pages/LoginPage.tsx
+++ frontend/js/pages/LoginPage.tsx
@@ -3,7 +3,6 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router";
 import { z } from "zod";
 
-import type { Login } from "../api";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -15,6 +14,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+import type { Login } from "../api";
 import { useAuth } from "../contexts/AuthContext";
 
 type LoginFormValues = Login;
