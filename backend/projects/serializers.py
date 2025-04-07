44713c3f52fc843313301c4f4eebbc0ba34fcab9
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-07 17:35:30 +0100

    feat: update ProjectSerializer to include write-only database_id field

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
+from databases.models import Database
 from queries.serializers import QuerySerializer
 from .models import Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    database = serializers.StringRelatedField()
+    database_id = serializers.PrimaryKeyRelatedField(
+        queryset=Database.objects.all(),
+        source='database',
+        write_only=True
+    )
+    database = serializers.StringRelatedField(read_only=True)
     queries = QuerySerializer(many=True, read_only=True)
 
     class Meta:
