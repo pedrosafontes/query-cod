bef04c0046151d58b1be8e55b0cb10bdc6d62687
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-09 13:18:15 +0100

    test: add pytest tests for project listing and creation functionality

---
+++ backend/projects/tests/test_views.py
@@ -0,0 +1,43 @@
+from django.urls import reverse
+
+import pytest
+from model_bakery import baker
+from projects.models import Project
+from rest_framework import status
+
+
+@pytest.mark.django_db
+def test_list_projects_returns_only_user_projects(auth_client, user):
+    user_project = baker.make(Project, user=user)
+
+    # Project belonging to someone else
+    other_user = baker.make("users.User")
+    baker.make(Project, user=other_user, _quantity=3)
+
+    url = reverse("projects-list")
+    response = auth_client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+
+    assert data["count"] == 1
+    assert len(data["results"]) == 1
+    assert data["results"][0]["id"] == user_project.id
+
+
+@pytest.mark.django_db
+def test_create_project_sets_user(auth_client, user):
+    url = reverse("projects-list")
+
+    payload = {
+        "name": "My new project",
+        "database_id": baker.make("databases.Database").id
+    }
+
+    response = auth_client.post(url, data=payload)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    project = Project.objects.get(name="My new project")
+
+    assert project.user == user
+    assert project.database.id == payload["database_id"]
