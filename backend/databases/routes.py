61281262639bf30676d88d867e58ec0886aa86a8
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-12 13:18:41 +0100

    feat: Add type annotations for route configurations

--- backend/databases/routes.py
+++ backend/databases/routes.py
@@ -1,6 +1,8 @@
+from common.types import RouteConfig
+
 from .views import DatabaseViewSet
 
 
-routes = [
+routes: list[RouteConfig] = [
     {'regex': r'databases', 'viewset': DatabaseViewSet, 'basename': 'databases'},
 ]
