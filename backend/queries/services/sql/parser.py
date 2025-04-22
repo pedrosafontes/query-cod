258e9b7f4637a9454abbbf857bbb0a8481ba4fe1
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-22 21:33:38 +0100

    refactor: remove DatabaseConnectionInfo dependency from parse_sql function

--- backend/queries/services/sql/parser.py
+++ backend/queries/services/sql/parser.py
@@ -1,11 +1,9 @@
-from databases.models import DatabaseConnectionInfo
 from sqlglot import expressions, parse_one
 
 
-def parse_sql(query_text: str, db: DatabaseConnectionInfo) -> expressions.Select:
+def parse_sql(query_text: str) -> expressions.Select:
     return parse_one(
         query_text,
-        read=_database_type_to_sqlglot(db.database_type),
         into=expressions.Select,
     )
 
