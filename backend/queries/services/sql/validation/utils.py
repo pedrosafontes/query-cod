ac6128c69fdb4f5ebfb17bb155a52dbb3b1a1f27
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-04-22 21:35:52 +0100

    feat: implement infer_literal_type function for type inference of SQL literals

--- backend/queries/services/sql/validation/utils.py
+++ backend/queries/services/sql/validation/utils.py
@@ -1,5 +1,33 @@
 import re
 
+import sqlglot.expressions as exp
+from databases.types import DataType
+
+
+def infer_literal_type(node: exp.Literal) -> DataType:
+    value = node.this
+
+    if node.is_int:
+        return DataType.INTEGER
+    elif node.is_number:
+        return DataType.FLOAT
+    elif node.is_string:
+        value = str(value).lower()
+        if value in {'true', 'false'}:
+            return DataType.BOOLEAN
+        elif value in {'null', 'none'}:
+            return DataType.NULL
+        elif is_date_format(value):
+            return DataType.DATE
+        elif is_time_format(value):
+            return DataType.TIME
+        elif is_timestamp_format(value):
+            return DataType.TIMESTAMP
+        else:
+            return DataType.VARCHAR
+    else:
+        raise ValueError(f'Unsupported literal: {node}')
+
 
 def is_date_format(s: str) -> bool:
     return bool(re.fullmatch(r'\d{4}-\d{2}-\d{2}', s))
