f08c3b351321d759593c04d8fadf0d0a92ee8f2e
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-04 18:32:09 +0100

    fix: update MathfieldElement mock to use jest.fn() for setOptions and getValue methods

--- frontend/js/__mocks__/mathlive.ts
+++ frontend/js/__mocks__/mathlive.ts
@@ -2,9 +2,24 @@ export class MathfieldElement {
   value = "";
 
   // eslint-disable-next-line class-methods-use-this, no-empty-function
-  setOptions() {}
+  setOptions = jest.fn();
 
   executeCommand = jest.fn();
 
-  getValue = () => this.value;
+  getValue = jest.fn();
+}
+
+if (!customElements.get("math-field")) {
+  customElements.define(
+    "math-field",
+    class extends HTMLElement {
+      value = "";
+
+      getValue = jest.fn();
+
+      setValue = jest.fn();
+
+      executeCommand = jest.fn();
+    },
+  );
 }
