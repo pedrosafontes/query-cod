480b3e59c136cc68f329aa9e5e913c12f9ff5c86
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-09 11:49:16 +0100

    refactor: remove unused 'act' import from useSchemaDiagram tests

--- frontend/js/components/database/useSchemaDiagram.spec.tsx
+++ frontend/js/components/database/useSchemaDiagram.spec.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act, waitFor } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 
 import { Database, DatabasesService } from "api";
 
