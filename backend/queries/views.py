389a74831c1a28b839248a7a22159b8872fae78b
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-10 15:56:50 +0100

    feat: implement execute_subquery method to handle subquery execution

--- backend/queries/views.py
+++ backend/queries/views.py
@@ -1,6 +1,7 @@
 from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404
 
+from databases.types import QueryResult
 from drf_spectacular.utils import OpenApiParameter, extend_schema
 from projects.models import Project
 from rest_framework import mixins, viewsets
@@ -11,7 +12,7 @@ from rest_framework.serializers import BaseSerializer
 
 from .models import Query
 from .serializers import QueryExecutionSerializer, QuerySerializer
-from .services.execution import execute_query
+from .services.execution import execute_query, execute_subquery
 
 
 @extend_schema(
@@ -52,8 +53,28 @@ class QueryViewSet(
     def execute(self, request: Request, pk: str) -> Response:
         query = self.get_object()
         results = execute_query(query)
+        return self._handle_execution(results)
+
+    @extend_schema(
+        request=None,
+        responses=QueryExecutionSerializer,
+        parameters=[
+            OpenApiParameter(
+                name='subquery_id',
+                type=int,
+                location=OpenApiParameter.PATH,
+                required=True,
+            ),
+        ],
+    )
+    @action(detail=True, methods=['post'], url_path='subqueries/(?P<subquery_id>[0-9]+)/executions')
+    def execute_subquery(self, request: Request, pk: str, subquery_id: str) -> Response:
+        query = self.get_object()
+        results = execute_subquery(query, int(subquery_id))
+        return self._handle_execution(results)
+
+    def _handle_execution(self, results: QueryResult | None) -> Response:
         if results:
             return Response({'success': True, 'results': results})
         else:
             return Response({'success': False})
-
