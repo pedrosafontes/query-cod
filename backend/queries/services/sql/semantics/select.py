73ed2a6f5936fc4a754a2e2a32a24f15fa6b2b27
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-21 19:21:45 +0100

    refactor: rename scope to select in SelectValidator for better readability

--- backend/queries/services/sql/semantics/select.py
+++ backend/queries/services/sql/semantics/select.py
@@ -17,52 +17,52 @@ from .table import TableValidator
 
 class SelectValidator:
     @staticmethod
-    def validate(scope: SelectScope) -> None:
-        SelectValidator.validate_from(scope)
-        SelectValidator.validate_joins(scope)
-        SelectValidator.validate_where(scope)
-        SelectValidator.validate_group_by(scope)
-        SelectValidator.validate_having(scope)
-        SelectValidator.validate_projection(scope)
-        SelectValidator.validate_order_by(scope)
+    def validate(select: SelectScope) -> None:
+        SelectValidator.validate_from(select)
+        SelectValidator.validate_joins(select)
+        SelectValidator.validate_where(select)
+        SelectValidator.validate_group_by(select)
+        SelectValidator.validate_having(select)
+        SelectValidator.validate_projection(select)
+        SelectValidator.validate_order_by(select)
 
     @staticmethod
-    def validate_from(scope: SelectScope) -> None:
-        if scope.from_:
-            TableValidator(scope).validate(scope.from_.this)
+    def validate_from(select: SelectScope) -> None:
+        if select.from_:
+            TableValidator(select).validate(select.from_.this)
 
     @staticmethod
-    def validate_joins(scope: SelectScope) -> None:
-        join_validator = JoinValidator(scope)
-        for join in scope.joins:
+    def validate_joins(select: SelectScope) -> None:
+        join_validator = JoinValidator(select)
+        for join in select.joins:
             join_validator.validate(join)
 
     @staticmethod
-    def validate_where(scope: SelectScope) -> None:
-        if scope.where:
-            ExpressionValidator(scope).validate_boolean(
-                scope.where.this, ValidationContext(in_where=True)
+    def validate_where(select: SelectScope) -> None:
+        if select.where:
+            ExpressionValidator(select).validate_boolean(
+                select.where.this, ValidationContext(in_where=True)
             )
 
     @staticmethod
-    def validate_group_by(scope: SelectScope) -> None:
-        if scope.group:
-            for expr in scope.group.expressions:
-                ExpressionValidator(scope).validate(expr)
+    def validate_group_by(select: SelectScope) -> None:
+        if select.group:
+            for expr in select.group.expressions:
+                ExpressionValidator(select).validate(expr)
 
     @staticmethod
-    def validate_having(scope: SelectScope) -> None:
-        if scope.having:
-            ExpressionValidator(scope).validate_boolean(
-                scope.having.this, ValidationContext(in_having=True)
+    def validate_having(select: SelectScope) -> None:
+        if select.having:
+            ExpressionValidator(select).validate_boolean(
+                select.having.this, ValidationContext(in_having=True)
             )
 
     @staticmethod
-    def validate_projection(scope: SelectScope) -> None:
+    def validate_projection(select: SelectScope) -> None:
         table_aliases: dict[str | None, set[str]] = defaultdict(set)
-        for expr in cast(list[Expression], scope.select.expressions):
+        for expr in cast(list[Expression], select.query.expressions):
             if expr.is_star:
-                StarValidator(scope).validate(expr)
+                StarValidator(select).validate(expr)
                 continue
 
             alias = expr.alias_or_name
@@ -71,12 +71,12 @@ class SelectValidator:
                 raise DuplicateAliasError(expr)
             table_aliases[table].add(alias)
 
-            if expr.unalias() not in scope.group_by:  # type: ignore[no-untyped-call]
-                ExpressionValidator(scope).validate(expr, ValidationContext(in_select=True))
+            if expr.unalias() not in select.group_by:  # type: ignore[no-untyped-call]
+                ExpressionValidator(select).validate(expr, ValidationContext(in_select=True))
 
     @staticmethod
-    def validate_order_by(scope: SelectScope) -> None:
-        if scope.order_by:
-            order_by_validator = OrderByValidator(scope)
-            for expr in scope.order_by.expressions:
+    def validate_order_by(select: SelectScope) -> None:
+        if select.order_by:
+            order_by_validator = OrderByValidator(select)
+            for expr in select.order_by.expressions:
                 order_by_validator.validate(expr)
