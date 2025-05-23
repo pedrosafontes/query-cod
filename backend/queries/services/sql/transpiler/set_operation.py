e8b04158bad88ab770e022dc4cdf3efa67f62929
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-23 18:03:48 +0100

    feat: support correlated set operations

--- backend/queries/services/sql/transpiler/set_operation.py
+++ backend/queries/services/sql/transpiler/set_operation.py
@@ -1,16 +1,36 @@
-from queries.services.ra.parser.ast import SetOperation as RASetOperation
+from queries.services.ra.parser.ast import (
+    Attribute,
+    RAQuery,
+    cartesian,
+)
+from queries.services.ra.parser.ast import (
+    SetOperation as RASetOperation,
+)
+from queries.services.sql.transpiler.context import ContextRelationInferrer
 from sqlglot.expressions import Except, Intersect, Union
 
-from ..scope.query import SetOperationScope
+from ..scope.query import SetOperationScope, SQLScope
 
 
 class SetOperationTranspiler:
     @staticmethod
     def transpile(scope: SetOperationScope) -> RASetOperation:
-        from .query import QueryTranspiler
+        left, l_context_relations, l_parameters = SetOperationTranspiler.transpile_operand(
+            scope.left
+        )
+        right, r_context_relations, r_parameters = SetOperationTranspiler.transpile_operand(
+            scope.right
+        )
 
-        left = QueryTranspiler.transpile(scope.left)
-        right = QueryTranspiler.transpile(scope.right)
+        parameters = list(set(l_parameters) | set(r_parameters))
+
+        if parameters:
+            left = cartesian([left, *(r_context_relations - l_context_relations)]).project(
+                parameters, append=True
+            )
+            right = cartesian([right, *(l_context_relations - r_context_relations)]).project(
+                parameters, append=True
+            )
 
         match scope.set_operation:
             case Union():
@@ -21,3 +41,12 @@ class SetOperationTranspiler:
                 return left.difference(right)
             case _:
                 raise NotImplementedError(f'Unsupported set operation: {type(scope.set_operation)}')
+
+    @staticmethod
+    def transpile_operand(scope: SQLScope) -> tuple[RAQuery, set[RAQuery], list[Attribute]]:
+        from .query import QueryTranspiler
+
+        context_relations, parameters = ContextRelationInferrer(scope).infer(scope.query)
+        transpiled_query = QueryTranspiler.transpile(scope)
+
+        return transpiled_query, set(context_relations), parameters
