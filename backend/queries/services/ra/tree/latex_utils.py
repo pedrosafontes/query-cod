15b58e4599dd6125689833d71fb9d9a177a9be33
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-24 14:31:35 +0100

    refactor: simplify condition handling in LaTeX conversion by using pattern matching and removing obsolete operators

--- backend/queries/services/ra/tree/latex_utils.py
+++ backend/queries/services/ra/tree/latex_utils.py
@@ -1,12 +1,18 @@
 from queries.services.ra.parser.ast import (
+    EQ,
+    GT,
+    GTE,
+    LT,
+    LTE,
+    NEQ,
+    And,
     Attribute,
     BinaryBooleanExpression,
-    BinaryBooleanOperator,
     BooleanExpression,
     Comparison,
-    ComparisonOperator,
     ComparisonValue,
-    NotExpression,
+    Not,
+    Or,
 )
 
 
@@ -16,30 +22,24 @@ def convert_attribute(attr: Attribute) -> str:
 
 def convert_condition(condition: BooleanExpression) -> str:
     match condition:
-        case BinaryBooleanExpression(operator=op):
-            match op:
-                case BinaryBooleanOperator.AND:
-                    operator_label = LAND
-                case BinaryBooleanOperator.OR:
-                    operator_label = LOR
-            return f'({convert_condition(condition.left)} {operator_label} {convert_condition(condition.right)})'
-        case NotExpression():
-            return f'{LNOT} ({convert_condition(condition.expression)})'
-        case Comparison(operator=op):
-            match op:
-                case ComparisonOperator.EQUAL:
-                    operator_label = '='
-                case ComparisonOperator.NOT_EQUAL:
-                    operator_label = NEQ
-                case ComparisonOperator.GREATER_THAN:
-                    operator_label = '>'
-                case ComparisonOperator.GREATER_THAN_EQUAL:
-                    operator_label = GEQ
-                case ComparisonOperator.LESS_THAN:
-                    operator_label = '<'
-                case ComparisonOperator.LESS_THAN_EQUAL:
-                    operator_label = LEQ
-            return f'({_convert_value(condition.left)} {operator_label} {_convert_value(condition.right)})'
+        case BinaryBooleanExpression():
+            bool_exprs = {
+                And: AND_LATEX,
+                Or: OR_LATEX,
+            }
+            return f'({convert_condition(condition.left)} {bool_exprs[type(condition)]} {convert_condition(condition.right)})'
+        case Not():
+            return f'{NOT_LATEX} ({convert_condition(condition.expression)})'
+        case Comparison() as comp:
+            comps = {
+                EQ: '=',
+                NEQ: NEQ_LATEX,
+                GT: '>',
+                LT: '<',
+                GTE: GEQ_LATEX,
+                LTE: LEQ_LATEX,
+            }
+            return f'({_convert_value(comp.left)} {comps[type(comp)]} {_convert_value(comp.right)})'
         case Attribute() as attr:
             return convert_attribute(attr)
         case _:
@@ -74,9 +74,9 @@ def convert_text(text: str) -> str:
     return f'\\text{{{text}}}'
 
 
-LAND = '\\land'
-LOR = '\\lor'
-LNOT = '\\lnot'
-NEQ = '\\neq'
-LEQ = '\\leq'
-GEQ = '\\geq'
+AND_LATEX = '\\land'
+OR_LATEX = '\\lor'
+NOT_LATEX = '\\lnot'
+NEQ_LATEX = '\\neq'
+LEQ_LATEX = '\\leq'
+GEQ_LATEX = '\\geq'
