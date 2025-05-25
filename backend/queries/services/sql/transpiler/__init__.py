923752970ee393e0790ab1bda538ce4371f835b3
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-25 11:18:51 +0100

    feat: integrate SQL query normalisation in transpilation process

--- backend/queries/services/sql/transpiler/__init__.py
+++ backend/queries/services/sql/transpiler/__init__.py
@@ -2,6 +2,7 @@ from queries.services.ra.parser.ast import RAQuery
 from queries.services.types import RelationalSchema, SQLQuery
 
 from ..scope.builder import build_scope
+from .normaliser import SQLQueryNormaliser
 from .query import QueryTranspiler
 
 
@@ -10,5 +11,6 @@ class SQLtoRATranspiler:
         self.schema = schema
 
     def transpile(self, query: SQLQuery) -> RAQuery:
+        query = SQLQueryNormaliser.normalise(query)
         scope = build_scope(query, self.schema)
         return QueryTranspiler.transpile(scope)
