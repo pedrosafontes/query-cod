89e752136ecaf41c110eebed9d98f0b053bac187
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-26 17:44:55 +0100

    feat: enhance RALatexConverter to support Unary and Binary operations in LaTeX conversion

--- backend/queries/services/ra/latex/converter.py
+++ backend/queries/services/ra/latex/converter.py
@@ -10,6 +10,7 @@ from ..parser.ast import (
     And,
     Attribute,
     BinaryBooleanExpression,
+    BinaryOperation,
     BooleanExpression,
     Comparison,
     ComparisonValue,
@@ -28,6 +29,7 @@ from ..parser.ast import (
     SetOperator,
     ThetaJoin,
     TopN,
+    UnaryOperation,
 )
 from . import utils as latex
 from .utils import overset, paren, subscript, text
@@ -39,7 +41,31 @@ class RALatexConverter:
         method: Callable[[RAQuery], str] = getattr(
             RALatexConverter, f'_convert_{type(query).__name__}'
         )
-        return method(query)
+        if isinstance(query, UnaryOperation):
+            return method(query) + RALatexConverter._convert_unary_operand(query.subquery)
+        elif isinstance(query, BinaryOperation):
+            return (
+                RALatexConverter._convert_binary_operand(query.left)
+                + method(query)
+                + RALatexConverter._convert_binary_operand(query.right)
+            )
+        else:
+            # Relation
+            return method(query)
+
+    @staticmethod
+    def _convert_unary_operand(subquery: RAQuery) -> str:
+        if isinstance(subquery, Relation | UnaryOperation):
+            return RALatexConverter.convert(subquery)
+        else:
+            return paren(RALatexConverter.convert(subquery))
+
+    @staticmethod
+    def _convert_binary_operand(subquery: RAQuery) -> str:
+        if isinstance(subquery, Relation):
+            return RALatexConverter.convert(subquery)
+        else:
+            return paren(RALatexConverter.convert(subquery))
 
     @staticmethod
     def _convert_Relation(rel: Relation) -> str:  # noqa: N802
@@ -48,24 +74,19 @@ class RALatexConverter:
     @staticmethod
     def _convert_Projection(proj: Projection) -> str:  # noqa: N802
         attributes = [RALatexConverter.convert_attribute(attr) for attr in proj.attributes]
-        return subscript(latex.PI, ', '.join(attributes)) + paren(
-            RALatexConverter.convert(proj.subquery)
-        )
+        return subscript(latex.PI, ', '.join(attributes))
 
     @staticmethod
     def _convert_Selection(sel: Selection) -> str:  # noqa: N802
         condition = RALatexConverter.convert_condition(sel.condition)
-        return subscript(latex.SIGMA, condition) + paren(RALatexConverter.convert(sel.subquery))
+        return subscript(latex.SIGMA, condition)
 
     @staticmethod
     def _convert_Rename(rename: Rename) -> str:  # noqa: N802
-        return subscript(latex.RHO, rename.alias) + paren(RALatexConverter.convert(rename.subquery))
+        return subscript(latex.RHO, rename.alias)
 
     @staticmethod
     def _convert_SetOperation(set_op: SetOperation) -> str:  # noqa: N802
-        left = RALatexConverter.convert(set_op.left)
-        right = RALatexConverter.convert(set_op.right)
-
         operators: dict[SetOperator, str] = {
             SetOperator.UNION: latex.CUP,
             SetOperator.DIFFERENCE: latex.CAP,
@@ -73,35 +94,26 @@ class RALatexConverter:
             SetOperator.CARTESIAN: latex.TIMES,
         }
 
-        return paren(left) + operators[set_op.operator] + paren(right)
+        return operators[set_op.operator]
 
     @staticmethod
     def _convert_Join(join: Join) -> str:  # noqa: N802
-        left = RALatexConverter.convert(join.left)
-        right = RALatexConverter.convert(join.right)
-
         operators: dict[JoinOperator, str] = {
             JoinOperator.NATURAL: latex.JOIN,
             JoinOperator.SEMI: latex.LTIMES,
             JoinOperator.ANTI: latex.ANTIJOIN,
         }
 
-        return paren(left) + operators[join.operator] + paren(right)
+        return operators[join.operator]
 
     @staticmethod
     def _convert_ThetaJoin(join: ThetaJoin) -> str:  # noqa: N802
-        left = RALatexConverter.convert(join.left)
-        right = RALatexConverter.convert(join.right)
         condition = RALatexConverter.convert_condition(join.condition)
-        return paren(left) + overset(condition, latex.BOWTIE) + paren(right)
+        return overset(condition, latex.BOWTIE)
 
     @staticmethod
     def _convert_Division(div: Division) -> str:  # noqa: N802
-        return (
-            paren(RALatexConverter.convert(div.dividend))
-            + latex.DIV
-            + paren(RALatexConverter.convert(div.divisor))
-        )
+        return latex.DIV
 
     @staticmethod
     def _convert_GroupedAggregation(agg: GroupedAggregation) -> str:  # noqa: N802
@@ -121,21 +133,20 @@ class RALatexConverter:
             ]
         )
 
-        return subscript(latex.GAMMA, paren(f'{paren(group_by)}, {paren(aggregations)}')) + paren(
-            RALatexConverter.convert(agg.subquery)
-        )
+        return subscript(latex.GAMMA, paren(f'{paren(group_by)}, {paren(aggregations)}'))
 
     @staticmethod
     def _convert_TopN(top: TopN) -> str:  # noqa: N802
         RALatexConverter.convert_attribute(top.attribute)
         attr = RALatexConverter.convert_attribute(top.attribute)
-        return subscript(latex.TOP_N, paren(f'{top.limit}, {attr}')) + paren(
-            RALatexConverter.convert(top.subquery)
-        )
+        return subscript(latex.TOP_N, paren(f'{top.limit}, {attr}'))
 
     @staticmethod
     def convert_attribute(attr: Attribute) -> str:
-        return text(str(attr))
+        if attr.relation:
+            return f'{text(attr.relation)}.{text(attr.name)}'
+        else:
+            return text(attr.name)
 
     @staticmethod
     def convert_condition(condition: BooleanExpression) -> str:
