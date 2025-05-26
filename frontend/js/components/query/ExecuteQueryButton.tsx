15e02a21c0eab7a3e5b9b2adea99eda0e859dda2
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-26 11:00:42 +0100

    feat: add TranspileQueryButton component and integrate it into QueryPage

--- frontend/js/components/query/ExecuteQueryButton.tsx
+++ frontend/js/components/query/ExecuteQueryButton.tsx
@@ -1,4 +1,5 @@
 import { Play } from "lucide-react";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
@@ -7,31 +8,55 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { QueriesService, QueryResultData } from "api";
+import { useErrorToast } from "hooks/useErrorToast";
 
 type ExecuteQueryButtonProps = {
+  queryId: number;
   disabled: boolean;
-  loading: boolean;
   hasErrors: boolean;
-  handleExecuteQuery: () => Promise<void>;
+  setQueryResult: (result: QueryResultData | undefined) => void;
 };
 
 const ExecuteQueryButton = ({
+  queryId,
   disabled,
-  loading,
   hasErrors,
-  handleExecuteQuery,
+  setQueryResult,
 }: ExecuteQueryButtonProps) => {
+  const [isExecuting, setIsExecuting] = useState(false);
+  const toast = useErrorToast();
+
+  const executeQuery = async (): Promise<void> => {
+    setIsExecuting(true);
+    try {
+      const execution = await QueriesService.queriesExecutionsCreate({
+        id: queryId,
+      });
+
+      setQueryResult(execution.results);
+    } catch (err) {
+      toast({
+        title: "Error executing query",
+      });
+    } finally {
+      setIsExecuting(false);
+    }
+  };
+
   return (
     <Tooltip delayDuration={200}>
       <TooltipTrigger asChild>
         <div>
+          {" "}
+          {/* div ensures tooltip is displayed when button is disabled */}
           <Button
-            disabled={disabled}
+            disabled={disabled || isExecuting || hasErrors}
             size="sm"
             variant="default"
-            onClick={handleExecuteQuery}
+            onClick={executeQuery}
           >
-            {loading ? (
+            {isExecuting ? (
               <Spinner className="text-primary-foreground" size="small" />
             ) : (
               <Play />
