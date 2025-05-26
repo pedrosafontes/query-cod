ea4aeeb07070c7782ffa21ac98fd3b5faca72c1b
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-26 17:20:16 +0100

    feat: add movies_schema and schema definitions for SQL query normalization tests

---
+++ backend/queries/tests/services/sql/transpiler/schemas.py
@@ -0,0 +1,45 @@
+from ra_sql_visualisation.types import DataType
+
+
+movies_schema = {
+    'Movie': {
+        'title': DataType.VARCHAR,
+        'year': DataType.INTEGER,
+        'length': DataType.INTEGER,
+        'genre': DataType.VARCHAR,
+        'studioName': DataType.VARCHAR,
+        'producerC#': DataType.INTEGER,
+    },
+    'MovieStar': {
+        'name': DataType.VARCHAR,
+        'address': DataType.VARCHAR,
+        'gender': DataType.CHAR,
+        'birthdate': DataType.DATE,
+    },
+    'StarsIn': {
+        'movieTitle': DataType.VARCHAR,
+        'movieYear': DataType.INTEGER,
+        'starName': DataType.VARCHAR,
+    },
+    'MovieExec': {
+        'name': DataType.VARCHAR,
+        'address': DataType.VARCHAR,
+        'CERT#': DataType.INTEGER,
+        'netWorth': DataType.INTEGER,
+    },
+    'Studio': {
+        'name': DataType.VARCHAR,
+        'address': DataType.VARCHAR,
+        'presC#': DataType.INTEGER,
+    },
+}
+
+schema = {
+    'R': {
+        'A': DataType.INTEGER,
+        'B': DataType.INTEGER,
+    },
+    'S': {
+        'C': DataType.INTEGER,
+    },
+}
--- backend/queries/tests/services/sql/transpiler/test_normaliser.py
+++ backend/queries/tests/services/sql/transpiler/test_normaliser.py
@@ -1,10 +1,13 @@
 import pytest
 from queries.services.sql.parser import parse_sql
 from queries.services.sql.transpiler.normaliser import SQLQueryNormaliser
+from queries.services.types import RelationalSchema
+
+from .schemas import movies_schema, schema
 
 
 @pytest.mark.parametrize(
-    'query,expected',
+    'query,expected,schema',
     [
         # IN
         (
@@ -16,10 +19,11 @@ from queries.services.sql.transpiler.normaliser import SQLQueryNormaliser
             """,
             """
             SELECT movieTitle FROM StarsIn
-            WHERE EXISTS (SELECT name
+            WHERE EXISTS (SELECT MovieStar.name
                           FROM MovieStar
-                          WHERE birthdate = 1960 AND starName = name)
+                          WHERE MovieStar.birthdate = 1960 AND StarsIn.starName = MovieStar.name)
             """,
+            movies_schema,
         ),
         # NOT IN
         (
@@ -31,10 +35,11 @@ from queries.services.sql.transpiler.normaliser import SQLQueryNormaliser
             """,
             """
             SELECT movieTitle FROM StarsIn
-            WHERE NOT EXISTS (SELECT name
+            WHERE NOT EXISTS (SELECT MovieStar.name
                               FROM MovieStar
-                              WHERE birthdate = 1960 AND starName = name)
+                              WHERE MovieStar.birthdate = 1960 AND StarsIn.starName = MovieStar.name)
             """,
+            movies_schema,
         ),
         # ALL
         (
@@ -47,8 +52,9 @@ from queries.services.sql.transpiler.normaliser import SQLQueryNormaliser
             SELECT name FROM MovieExec
             WHERE NOT EXISTS(SELECT E.netWorth
                              FROM MovieExec E
-                             WHERE netWorth < E.netWorth)
+                             WHERE MovieExec.netWorth < E.netWorth)
             """,
+            movies_schema,
         ),
         # ANY
         (
@@ -61,8 +67,9 @@ from queries.services.sql.transpiler.normaliser import SQLQueryNormaliser
             SELECT name FROM MovieExec
             WHERE EXISTS(SELECT E.netWorth
                          FROM MovieExec E
-                         WHERE netWorth >= E.netWorth)
+                         WHERE MovieExec.netWorth >= E.netWorth)
             """,
+            movies_schema,
         ),
         # IN - Grouped Subquery
         (
@@ -73,10 +80,11 @@ from queries.services.sql.transpiler.normaliser import SQLQueryNormaliser
             """,
             """
             SELECT C FROM S
-            WHERE EXISTS (SELECT SUM(B) FROM R
-                          GROUP BY A
-                          HAVING C = SUM(B))
+            WHERE EXISTS (SELECT SUM(R.B) FROM R
+                          GROUP BY R.A
+                          HAVING S.C = SUM(R.B))
             """,
+            schema,
         ),
         # No quantifier
         (
@@ -87,46 +95,23 @@ from queries.services.sql.transpiler.normaliser import SQLQueryNormaliser
             """,
             """
             SELECT C FROM S
-            WHERE EXISTS (SELECT SUM(B) FROM R
-                          GROUP BY A
-                          HAVING C > SUM(B))
+            WHERE EXISTS (SELECT SUM(R.B) FROM R
+                          GROUP BY R.A
+                          HAVING S.C > SUM(R.B))
             """,
-        ),
-        # Subquery with set operation
-        (
-            """
-            SELECT movieTitle FROM StarsIn
-            WHERE starName IN (SELECT name
-                               FROM MovieStar
-                               WHERE birthdate = 1960
-                               UNION
-                               SELECT name
-                               FROM MovieStar
-                               WHERE birthdate = 1970)
-            """,
-            """
-            SELECT movieTitle FROM StarsIn
-            WHERE EXISTS (SELECT *
-                          FROM (SELECT name
-                                FROM MovieStar
-                                WHERE birthdate = 1960
-                                UNION
-                                SELECT name
-                                FROM MovieStar
-                                WHERE birthdate = 1970) AS sub
-                          WHERE starName = name)
-            """,
+            schema,
         ),
     ],
 )
 def test_subquery_normalisation(
     query: str,
     expected: str,
+    schema: RelationalSchema,
 ) -> None:
     test_query = parse_sql(query)
     expected_query = parse_sql(expected)
 
-    normalised_query = SQLQueryNormaliser.normalise_subqueries(test_query)
+    normalised_query = SQLQueryNormaliser.normalise_subqueries(test_query, schema)
     print(normalised_query.sql(pretty=True))
 
     assert normalised_query == expected_query
@@ -178,3 +163,37 @@ def test_condition_normalisation(
     print(normalised_query.sql(pretty=True))
 
     assert normalised_query == expected_query
+
+
+@pytest.mark.parametrize(
+    'query,expected',
+    [
+        (
+            """
+            SELECT * FROM R, R
+            """,
+            """
+            SELECT * FROM R, R AS R1
+            """,
+        ),
+        (
+            """
+            SELECT * FROM R, (SELECT R.A FROM R) AS R1
+            """,
+            """
+            SELECT * FROM R, (SELECT R2.A FROM R AS R2) AS R1
+            """,
+        ),
+    ],
+)
+def test_table_aliasing(
+    query: str,
+    expected: str,
+) -> None:
+    test_query = parse_sql(query)
+    expected_query = parse_sql(expected)
+
+    normalised_query = SQLQueryNormaliser.alias_tables(test_query)
+    print(normalised_query.sql(pretty=True))
+
+    assert normalised_query.sql() == expected_query.sql()
