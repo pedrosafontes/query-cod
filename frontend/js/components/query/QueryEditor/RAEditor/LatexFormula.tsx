62761091e57b8b506afc5aad788d27cc6884b96e
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-05-30 17:55:35 +0100

    fix: correct formatting and consistency in convertDisplayLinesToAligned function

--- frontend/js/components/query/QueryEditor/RAEditor/LatexFormula.tsx
+++ frontend/js/components/query/QueryEditor/RAEditor/LatexFormula.tsx
@@ -10,14 +10,14 @@ type LatexFormulaProps = {
 function convertDisplayLinesToAligned(input: string): string {
   if (!input.startsWith("\\displaylines{")) return input;
 
-  console.log(input)
+  console.log(input);
 
   const content = input.slice(14, -1); // Extract content inside \displaylines{}
 
   const alignedContent = content
-    .split('\\\\')
-    .map(line => `& ${line}`)
-    .join('\\\\');
+    .split("\\\\")
+    .map((line) => `& ${line}`)
+    .join("\\\\");
 
   return `\\begin{align*}\n${alignedContent}\n\\end{align*}`;
 }
